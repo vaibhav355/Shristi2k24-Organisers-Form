6e13be3abb2cc1ca18a3a48a4fbefcdb3fc1ad8c
Vishal Vaibhav <vaibhavishal81@gmail.com>
2024-04-05 18:39:55 +0530

    Commented Select

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 import "./Form.css"; // Import your CSS file for styling
 
 import { prodUrl } from "./config";
+import clubs from "./data";
+
 
 const Form = () => {
   const [loading, setLoading] = useState(false);
+
+  // console.log(...clubs)
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,7 +21,7 @@ const Form = () => {
     formData.append("position", e.target.position.value);
 
     setLoading(true);
-    fetch(prodUrl + "/orgnisers", {
+    fetch(prodUrl + "/organiser", {
       method: "POST",
       body: formData,
     })
@@ -31,9 +35,8 @@ const Form = () => {
       });
   };
 
-
   const handleOnChange = (e) => {
-    if (e.target.files[0].size > 1 * 1000 * 1024 && e.target.files[0]) {
+    if (e.target.files[0]?.size > 1 * 1000 * 1024 && e.target.files[0]) {
       alert("File is too big! Please select a file smaller than 1 MB.!");
       e.target.value = null; //clear the input
       return false;
@@ -42,78 +45,91 @@ const Form = () => {
 
   return (
     <div className="form-div">
-    <div className="form-container">
-      <h1 className="form-heading">About</h1>
-      <div className="card">
-        <div className="card-content">
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Enter Name"
-                autoComplete="off"
-              />
-            </div>
+      <h1 className="form-heading">Shristi 2k24</h1>
+      <h1 className="form-heading">Organisers</h1>
+      <div className="form-container">
+        <div className="card">
+          <div className="card-content">
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="name">Name:</label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Enter Name"
+                  autoComplete="off"
+                  required
+                />
+              </div>
 
-            <div className="form-group">
-              <label htmlFor="pic">Image:</label>
-              <input
-                type="file"
-                accept="image/*"
-                id="pic"
-                name="pic"
-                onChange={handleOnChange}
-              />
-            </div>
+              <div className="form-group">
+                <label htmlFor="pic">Image:</label>
+                <input
+                  type="file"
+                  accept="image/*"
+                  id="pic"
+                  name="pic"
+                  onChange={handleOnChange}
+                  required
+                />
+              </div>
 
-            <div className="form-group">
-              <label htmlFor="regno">Reg No:</label>
-              <input
-                type="text"
-                name="regno"
-                id="regno"
-                placeholder="Enter Registration Number"
-                autoComplete="off"
-              />
-            </div>
+              <div className="form-group">
+                <label htmlFor="regno">Reg No:</label>
+                <input
+                  type="text"
+                  name="regno"
+                  id="regno"
+                  placeholder="Enter Reg. Number e.g. 330/089"
+                  autoComplete="off"
+                  required
+                />
+              </div>
 
-            <div className="form-group">
-              <label htmlFor="group">Group:</label>
-              <input
-                type="text"
-                name="group"
-                id="group"
-                placeholder="Enter Your Group"
-                autoComplete="off"
-              />
-            </div>
+              <div className="form-group">
+                <label htmlFor="group">Group:</label>
+                <input
+                  type="text"
+                  name="  "
+                  id="group"
+                  placeholder="Enter Your Group Name"
+                  autoComplete="off"
+                  required
+                />
+                {/* <select name="group" id="group" required>
+                  {clubs.map((club, index) => {
+                    return(
+                      <option key={index} value={club.name}>{club.name}</option>
+                    )
+                  })}
+                </select> */}
+              </div>
 
-            <div className="form-group">
-              <label htmlFor="position">Position:</label>
-              <input
-                type="text"
-                name="position"
-                id="position"
-                placeholder="Enter Your Position"
-                autoComplete="off"
-              />
-            </div>
+              <div className="form-group">
+                <label htmlFor="position">Position:</label>
+                <input
+                  type="text"
+                  name="position"
+                  id="position"
+                  placeholder="Enter Your Position"
+                  autoComplete="off"
+                  required
+                />
+              </div>
 
-            <button type="submit" className="submit-button">
-              Submit
-            </button>
-          </form>
+              <button type="submit" className="submit-button">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
+        {loading && (
+          <div className="backdrop">
+            <div className="loader">Loading...</div>
+          </div>
+        )}
       </div>
-      {loading && (
-        <div className="backdrop">
-          <div className="loader">Loading...</div>
-        </div>
-      )}
-    </div>
     </div>
   );
 };
